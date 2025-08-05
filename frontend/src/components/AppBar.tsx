6b1026b1651aaaaccb5910e29df4ee8d1b81f051
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-08-05 21:05:00 +0530

    icon and signin

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -4,13 +4,13 @@ export const AppBar = () => {
     return <div className="flex justify-between 
     pt-1">
         <div className="text-md inline-flex items-center pb-2">
-            Youtube
+            <img src="you.jpg" className="cursor-pointer w-30 h-10" ></img>
         </div>
         <div>
             <SearchBar></SearchBar>
         </div>
         <div>
-            Sign in
+            <img src="sign.png" className="cursor-pointer w-33 h-14 pt-1"></img>
         </div>
     </div>
 }
